9bd621b66e881f677d1e94537b0a9f804ed47802
Mark Wolfe <mark@wolfe.id.au>
2013-03-30 21:46:32 +1100

    Added rrd wrapper.

--- test/integration/rrd.js
+++ test/integration/rrd.js
@@ -7,16 +7,25 @@ var expect = chai.expect;
 
 describe('RRD', function () {
 
-
-
-  it('should return a version string', function(done){
+  it('should return a version string', function (done) {
 
     var rrd = new RRDTool();
 
-    rrd.version(function(err, data){
-
+    rrd.version(function (err, data) {
       expect(err).to.not.exist;
       expect(data).to.exist;
+      done();
+    });
+
+  });
+
+  it('should return an error with bad command path', function (done) {
+
+    var rrd = new RRDTool({path: 'bad_command'});
+
+    rrd.version(function (err, data) {
+      expect(err).to.exist;
+      expect(data).to.not.exist;
       done();
     });
 
