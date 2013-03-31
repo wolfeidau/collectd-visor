899b3c4e8038f79a775dbc3d5c41eb207fe829a3
Mark Wolfe <mark@wolfe.id.au>
2013-03-31 11:15:08 +1100

    Added rrd info call.

--- test/integration/rrd.js
+++ test/integration/rrd.js
@@ -31,4 +31,23 @@ describe('RRD', function () {
 
   });
 
+  it('should return info for a rrd file', function (done) {
+
+    var rrd = new RRDTool();
+
+    rrd.info('./data/collectd/rrd/ubuntu1204-2server01/load/load.rrd', function (err, data) {
+      expect(err).to.not.exist;
+      // {
+      // shortterm: { index: 0, type: 'GAUGE', min: 0, max: 100, value: 0 }
+      // , midterm: { index: 1, type: 'GAUGE', min: 0, max: 100, value: 0.03 }
+      // , longterm: { index: 2, type: 'GAUGE', min: 0, max: 100, value: 0.15 }
+      // }
+      expect(data['shortterm']['index']).to.exist;
+      expect(data['midterm']['index']).to.exist;
+      expect(data['longterm']['index']).to.exist;
+      done();
+    });
+
+  });
+
 });
